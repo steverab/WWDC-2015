e1bfeafd8d5ae1ad4e4aad135a02b3fcb3fcfa95
Stephan Rabanser <steverab93@gmail.com>
2015-04-20 01:08:24 +0200

    Add basic parallax effect

--- Stephan Rabanser/TimelineViewController.swift
+++ Stephan Rabanser/TimelineViewController.swift
@@ -11,6 +11,16 @@ import UIKit
 class TimelineViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var timelineTableView: UITableView!
+    @IBOutlet var header:UIView!
+    @IBOutlet var headerLabel:UILabel!
+    
+    var headerImageView:UIImageView!
+    var headerBlurImageView:UIImageView!
+    var blurredHeaderImageView:UIImageView?
+    
+    let offsetHeaderStop:CGFloat = 60.0
+    let offsetLabelHeader:CGFloat = 65.0
+    let blurFadeDuration:CGFloat = 35.0
     
     var entries = [TimelineEntry]()
     
@@ -22,6 +32,7 @@ class TimelineViewController: UIViewController, UITableViewDataSource, UITableVi
         navigationController?.interactivePopGestureRecognizer.delegate = nil
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .Plain, target: nil, action: nil)
         
+        setupHeader()
         setupTableView()
         loadEntries()
     }
@@ -29,14 +40,29 @@ class TimelineViewController: UIViewController, UITableViewDataSource, UITableVi
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: true)
-        UIApplication.sharedApplication().setStatusBarStyle(.Default, animated: true)
+        UIApplication.sharedApplication().setStatusBarStyle(.LightContent, animated: true)
     }
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
-        return UIStatusBarStyle.Default
+        return UIStatusBarStyle.LightContent
     }
     
     // MARK: - Functions
+    
+    func setupHeader() {
+        headerImageView = UIImageView(frame: header.frame)
+        headerImageView?.image = UIImage(named: "Singapore")
+        headerImageView?.contentMode = UIViewContentMode.ScaleAspectFill
+        header.insertSubview(headerImageView, belowSubview: headerLabel)
+        
+        headerBlurImageView = UIImageView(frame: header.frame)
+        headerBlurImageView?.image = UIImage(named: "Singapore")?.blurredImageWithRadius(10, iterations: 20, tintColor: UIColor.clearColor())
+        headerBlurImageView?.contentMode = UIViewContentMode.ScaleAspectFill
+        headerBlurImageView?.alpha = 0.0
+        header.insertSubview(headerBlurImageView, belowSubview: headerLabel)
+        
+        header.clipsToBounds = true
+    }
     
     func setupTableView() {
         timelineTableView.dataSource = self
@@ -46,6 +72,8 @@ class TimelineViewController: UIViewController, UITableViewDataSource, UITableVi
         
         timelineTableView.rowHeight = UITableViewAutomaticDimension
         timelineTableView.estimatedRowHeight = 44
+        
+        timelineTableView.contentInset = UIEdgeInsetsMake(105, 0, 0, 0)
     }
     
     func loadEntries() {
@@ -54,6 +82,30 @@ class TimelineViewController: UIViewController, UITableViewDataSource, UITableVi
             let entry = TimelineEntry(title: dict["title"] as! String, shortDescription: dict["shortDescription"] as! String, description: dict["description"] as! String, date: dict["date"] as! String, type: TimelineEntryType(rawValue: dict["type"] as! Int)!, imageString: dict["image"] as! String)
             entries.append(entry)
         }
+    }
+    
+    // MARL: - Scroll view delegate
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let offset = scrollView.contentOffset.y + timelineTableView.contentInset.top
+        var avatarTransform = CATransform3DIdentity
+        var headerTransform = CATransform3DIdentity
+        
+        if offset < 0 {
+            let headerScaleFactor:CGFloat = -(offset) / header.bounds.height
+            let headerSizevariation = ((header.bounds.height * (1.0 + headerScaleFactor)) - header.bounds.height)/2.0
+            headerTransform = CATransform3DTranslate(headerTransform, 0, headerSizevariation, 0)
+            headerTransform = CATransform3DScale(headerTransform, 1.0 + headerScaleFactor, 1.0 + headerScaleFactor, 0)
+        } else {
+            headerTransform = CATransform3DTranslate(headerTransform, 0, max(-offsetHeaderStop, -offset), 0)
+            
+            let labelTransform = CATransform3DMakeTranslation(0, max(-blurFadeDuration, offsetLabelHeader - offset), 0)
+            headerLabel.layer.transform = labelTransform
+            
+            headerBlurImageView?.alpha = min (1.0, (offset - offsetLabelHeader)/blurFadeDuration)
+        }
+        
+        header.layer.transform = headerTransform
     }
     
     // MARK: - Table view data source
